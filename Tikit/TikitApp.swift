6a4f603e6ce2b572500bcff049ab43013e99097e
Mauricio Ampuero <mdampuero@gmail.com>
2025-08-30 17:57:08 +0200

    Initial Commit

---
+++ Tikit/TikitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikitApp.swift
+//  Tikit
+//
+//  Created by Mauricio Ampuero on 30/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TikitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
