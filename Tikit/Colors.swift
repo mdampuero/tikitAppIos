b442e378c3f623047c5b4a967ef7ec97f6cd4c26
Mauricio Ampuero <mdampuero@gmail.com>
2025-09-01 14:22:07 +0200

    Define brand color constants

---
+++ Tikit/Colors.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+extension Color {
+    /// Primary brand color (#B6287E)
+    static let brandPrimary = Color(red: 182/255.0, green: 40/255.0, blue: 126/255.0)
+    /// Secondary brand color (#5E38E2)
+    static let brandSecondary = Color(red: 94/255.0, green: 56/255.0, blue: 226/255.0)
+}
